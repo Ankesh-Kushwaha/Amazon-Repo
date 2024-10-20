599d179bc877961969a80354c0bc8b8d75a63e87
ankeshkushwaha <ankeshkush9651@gmail.com>
2024-10-20 23:59:22 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,63 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPayementSummary } from "./checkout/payementSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+         resolve('value1');
+    });
+}),
+
+new Promise((resolve)=>{
+  loadCart(()=>{
+      resolve();
+  });
+}),
+
+]).then((values)=>{
+  console.log(values);
   renderOrderSummary();
   renderPayementSummary();
+})
+
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+         resolve('value1');
+    });
+
+}).then((value)=>{
+  console.log(value);
+     return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+     });
+
+}).then(()=>{
+  renderOrderSummary();
+  renderPayementSummary();
+})
+*/
+
+
+/*in both case there is two steps
+1. first we load the products and wait for them to finish using resolve.
+2. the we call the renderOrderSummary() and
+   renderPayementSummary();
+*/
+
+
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPayementSummary();
+  })
+ 
 });
+*/
