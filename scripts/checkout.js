db729ddc7a2a11659bcb5c6067ca33462b42d6f4
ankeshkushwaha <ankeshkush9651@gmail.com>
2024-10-20 14:48:16 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPayementSummary } from "./checkout/payementSummary.js";
+import '../data/cart-oop.js';
+
 renderOrderSummary();
 renderPayementSummary();
